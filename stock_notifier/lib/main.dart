eac3f55ba21797f70066b0da5908e42e4baa39c7
questras <michu123qwe@gmail.com>
2020-04-09 09:06:12 +0200

    app scratch

--- stock_notifier/lib/main.dart
+++ stock_notifier/lib/main.dart
@@ -1,111 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:stocknotifier/services/stock.dart' as stock;
+
+//void main(List<String> arguments) async {
+//  await stock.fetchStock();
+//}
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Welcome to Flutter',
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MainPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return StockList();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class StockListState extends State<StockList> {
+  Future<List<stock.Stock>> _futureStockList;
+  final Map<String, stock.Stock> _savedStocks = Map<String, stock.Stock>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _futureStockList = stock.fetchStock();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Stock prices'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {this._pushSaved();},
+          ),
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              this.setState(() {
+                _futureStockList = stock.fetchStock();
+              });
+            },
+          )
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+        child: this.createList(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  FutureBuilder<List<stock.Stock>> createList() {
+    return FutureBuilder<List<stock.Stock>>(
+      future: _futureStockList,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Widget> listTiles = new List<Widget>();
+
+          for (var element in snapshot.data) {
+            listTiles.add(createListTile(element, favorite: true));
+            listTiles.add(Divider());
+          }
+
+          return ListView(children: listTiles);
+        }
+        else if (snapshot.hasError) {
+
+          return Text('$snapshot.error');
+        }
+
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  ListTile createListTile(stock.Stock element, {favorite=false}) {
+    final bool alreadySaved = _savedStocks.containsKey(element.name);
+    return ListTile(
+      leading: Text(element.price),
+      title: Text(element.name),
+      trailing: favorite ?  Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ) : null,
+      onTap: () {
+        setState(() {
+          if (favorite) {
+            if (alreadySaved) {
+              _savedStocks.remove(element.name);
+            } else {
+              _savedStocks[element.name] = element;
+            }
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          List<Widget> listTiles = new List<Widget>();
+
+          for (var element in _savedStocks.keys) {
+            listTiles.add(createListTile(_savedStocks[element]));
+            listTiles.add(
+              ListTile(
+                leading: Text("Notify me at:"),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        initialValue: null,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            );
+            listTiles.add(Divider());
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved stock'),
+            ),
+            body: ListView(children: listTiles,),
+          );
+        }
+      )
+    );
+  }
+}
+
+class StockList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return StockListState();
   }
 }
---
+++ stock_notifier/lib/services/stock.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:html/parser.dart' as parser;
+import 'package:http/http.dart' as http;
+import 'package:html/dom.dart' as dom;
+
+class Stock {
+  final String name;
+  final String price;
+
+  Stock({this.name, this.price});
+}
+
+Future<List<Stock>> fetchStock() async {
+  var url = 'https://www.bankier.pl/gielda/notowania/akcje';
+  var client = http.Client();
+  http.Response response = await client.get(url);
+
+  var document = parser.parse(response.body);
+  List<dom.Element> items = document.querySelectorAll('tr > td');
+
+  List<Stock> stocks = [];
+  for (int i = 0; i < items.length; i++) {
+    if (items[i].attributes.containsKey('class')) {
+      if (items[i].attributes['class'] == 'colWalor textNowrap') {
+        var name = items[i].text.trim();
+        var price = items[i+1].text.trim();
+        stocks.add(Stock(name: name, price: price));
+      }
+    }
+  }
+
+  print("Success!");
+  return stocks;
+}
