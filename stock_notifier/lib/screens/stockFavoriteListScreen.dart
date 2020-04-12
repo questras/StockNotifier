7a7767745bc846e33e8f41bab7b846a5f60d12ec
questras <michu123qwe@gmail.com>
2020-04-12 11:00:31 +0200

    moved list tiles builders to screen class

--- stock_notifier/lib/screens/stockFavoriteListScreen.dart
+++ stock_notifier/lib/screens/stockFavoriteListScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:stocknotifier/components/icons.dart';
+import 'package:validators/validators.dart';
 
 import 'package:stocknotifier/services/sharedPreferencesHandler.dart';
-import 'package:stocknotifier/components/listTiles.dart';
+import 'package:stocknotifier/services/stock.dart';
 
 class StockFavoriteList extends StatefulWidget {
   @override
@@ -16,8 +18,6 @@ class StockFavoriteListState extends State<StockFavoriteList> {
   @override
   void initState() {
     super.initState();
-
-    _futurePreferencesKeys = getPreferences();
   }
 
   @override
@@ -31,31 +31,86 @@ class StockFavoriteListState extends State<StockFavoriteList> {
   }
 
   FutureBuilder<Set<String>> createFavoriteStockList() {
+    _futurePreferencesKeys = getPreferences();
     return FutureBuilder<Set<String>>(
       future: _futurePreferencesKeys,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<Widget> listTiles = new List<Widget>();
+      builder: (context, prefsSnapshot) {
+        if (prefsSnapshot.hasData) {
+          final _stockPrices = fetchStock();
+          return FutureBuilder<Map<String, Stock>>(
+            future: _stockPrices,
+            builder: (context, stockSnapshot) {
+              if (stockSnapshot.hasData) {
+                List<Widget> listTiles = new List<Widget>();
+                for (var key in prefsSnapshot.data) {
+                  listTiles
+                      .add(_stockListTile(key, stockSnapshot.data[key].price));
+                  listTiles.add(_notificationPriceListTile(key));
+                  listTiles.add(Divider());
+                }
 
-          for (var key in snapshot.data) {
-            listTiles.add(stockListTile(key, 0.0, _onFavoriteTap));
-            listTiles.add(notificationPriceListTile(key));
-            listTiles.add(Divider());
-          }
-
-          return ListView(children: listTiles);
-        } else if (snapshot.hasError) {
-          return Text('$snapshot.error');
+                return ListView(children: listTiles);
+              }
+              else if (stockSnapshot.hasError) {
+                return Text('${stockSnapshot.error}');
+              }
+              return CircularProgressIndicator();
+            },
+          );
+        }
+        else if (prefsSnapshot.hasError) {
+          return Text('$prefsSnapshot.error');
         }
         return CircularProgressIndicator();
       },
     );
   }
 
-  void _onFavoriteTap(bool alreadySaved, String name) async {
-    await preferencesRemove(name);
-     _futurePreferencesKeys = getPreferences();
-    setState(() {});
+  ListTile _stockListTile(String name, double price) {
+    return ListTile(
+      leading: Text(price.toString()),
+      title: Text(name),
+      trailing: favoriteIcon(true),
+      onTap: () {
+        preferencesRemove(name);
+        setState(() {});
+      },
+    );
+  }
+
+  FutureBuilder<double> _notificationPriceListTile(String name) {
+    Future<double> value = preferencesRead(name);
+    return FutureBuilder(
+      future: value,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListTile(
+            leading: Text('Notify me at:'),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    initialValue: snapshot.data.toString(),
+                    onChanged: (String number) {
+                      if (isFloat(number)) {
+                        preferencesSave(name, double.parse(number));
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+        return CircularProgressIndicator();
+      },
+    );
   }
 
 }
